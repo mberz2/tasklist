a8032ebf35bc70d213c5984c1100f2d99cbe26e8
mberz2 <56572702+mberz2@users.noreply.github.com>
2022-02-23 10:46:55 -0800

    Changes to card, list and board components.

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,8 +1,15 @@
 import React from "react";
+import List from "./List";
 
 class Board extends React.Component {
   render() {
-    return <p> board component </p>;
+    return (
+      <div>
+        <p> board component </p>
+        <List />
+        <List />
+      </div>
+    );
   }
 }
 
