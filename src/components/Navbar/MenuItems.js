69a969af5c0f2ba43a476832ad3f8bf27ce6d2b2
ntommy06 <tommyducnguyen@gmail.com>
2022-03-06 21:00:15 -0800

    added files for help and settings routes

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+  {
+    title: "Home",
+    url: "/",
+    cName: "nav-links",
+  },
+  {
+    title: "Help",
+    url: "/help",
+    cName: "nav-links",
+  },
+  {
+    title: "Settings",
+    url: "/settings",
+    cName: "nav-links",
+  },
+];
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./Navbar.css";
+import { MenuItems } from "./MenuItems";
+
+class Navbar extends Component {
+  state = { active: false };
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+  render() {
+    return (
+      <nav className="Navbar_items">
+        <h1 className="Navbar_logo">TaskBoard</h1>
+        <div className="menu-icon" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
