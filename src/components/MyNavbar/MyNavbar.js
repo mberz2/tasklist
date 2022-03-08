a7a3bd96800f8019fb695c33b13338a4bdd7280c
ntommy06 <tommyducnguyen@gmail.com>
2022-03-08 14:58:21 -0800

    completed setting the modals in the Navbar

--- src/components/MyNavbar/MyNavbar.js
+++ src/components/MyNavbar/MyNavbar.js
@@ -8,35 +8,94 @@ import {
   Modal,
   ModalBody,
 } from "react-bootstrap";
+import {
+  flattenDiagnosticMessageText,
+  isConstructorDeclaration,
+} from "typescript";
 // import { MenuItems } from "./MenuItems";
 
-const MyNavbar = () => {
-  return (
-    <div>
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand className="ml-auto">TaskBoard</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mx-auto">
-              {/* <Nav.Link href="#settings">Settings</Nav.Link> */}
-              {/* <Nav.Link href="/">Information</Nav.Link> */}
-              <Button>Information</Button>
-              <Modal show={false}>
-                <Modal.Header>Modal Head</Modal.Header>
-                <Modal.Body>Modal Body</Modal.Body>
-                <Modal.Footer>Modal Footer</Modal.Footer>
-              </Modal>
-              <Button>Settings</Button>
-              <a href="/">
-                <Button>Log-Out</Button>
-              </a>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
-  );
-};
+class MyNavbar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showModal1: false,
+      showModal2: false,
+    };
+  }
+
+  handleModal1() {
+    this.setState({ showModal1: !this.state.showModal1 });
+  }
+
+  handleModal2() {
+    this.setState({ showModal2: !this.state.showModal2 });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand className="ml-auto">TaskBoard</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mx-auto">
+                {/* Button for Information Modal =======*/}
+                <Button
+                  onClick={() => {
+                    this.handleModal1();
+                  }}
+                >
+                  Information
+                </Button>
+                <Modal show={this.state.showModal1}>
+                  <Modal.Header>Information</Modal.Header>
+                  <Modal.Body>Modal Body</Modal.Body>
+                  <Modal.Footer>
+                    <Button
+                      onClick={() => {
+                        this.handleModal1();
+                      }}
+                    >
+                      Close Modal
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+                {/* =================================== */}
+
+                {/* Button for Settings Modal =========*/}
+                <Button
+                  onClick={() => {
+                    this.handleModal2();
+                  }}
+                >
+                  Settings
+                </Button>
+                <Modal show={this.state.showModal2}>
+                  <Modal.Header>Settings</Modal.Header>
+                  <Modal.Body>Modal Body</Modal.Body>
+                  <Modal.Footer>
+                    <Button
+                      onClick={() => {
+                        this.handleModal2();
+                      }}
+                    >
+                      Close Modal
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+                {/* =================================== */}
+
+                <a href="/">
+                  <Button>Log-Out</Button>
+                </a>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
 
 export default MyNavbar;
