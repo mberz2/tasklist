ccdde36863806862a80d12aae0e8b1976db8784e
ntommy06 <tommyducnguyen@gmail.com>
2022-03-08 20:44:45 -0800

    Finished Navbar/Modals

--- src/components/MyNavbar/MyNavbar.js
+++ src/components/MyNavbar/MyNavbar.js
@@ -8,12 +8,10 @@ import {
   Modal,
   ModalBody,
   Accordion,
+  ListGroup,
+  ButtonGroup,
+  Alert,
 } from "react-bootstrap";
-// import {
-//   flattenDiagnosticMessageText,
-//   isConstructorDeclaration,
-// } from "typescript";
-// import { MenuItems } from "./MenuItems";
 
 class MyNavbar extends React.Component {
   constructor() {
@@ -35,13 +33,12 @@ class MyNavbar extends React.Component {
   render() {
     return (
       <div>
-        <Navbar bg="light" expand="lg">
-          <Container>
-            <Navbar.Brand className="ml-auto">TaskBoard</Navbar.Brand>
+        <Navbar id="navbar" bg="light" expand="sm">
+          <div class="container-fluid" style={{ margin: 0 }}>
+            <Navbar.Brand>TaskBoard</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mx-auto">
+              <Nav className="ml-auto">
                 {/* Button for Information Modal =======*/}
                 <Button
                   onClick={() => {
@@ -51,9 +48,18 @@ class MyNavbar extends React.Component {
                   Information
                 </Button>
                 <Modal size="lg" show={this.state.showModal1}>
-                  <Modal.Header>Information</Modal.Header>
+                  <Modal.Header>
+                    <h1>Information</h1>
+                  </Modal.Header>
                   <Modal.Body>
                     <Accordion>
+                      <Alert variant="info">
+                        <Alert.Heading>
+                          <b>Click items below!</b>
+                        </Alert.Heading>
+                        <hr />
+                        <p>Learn how to use our The TaskBoard Application!</p>
+                      </Alert>
                       <Accordion.Item eventKey="0">
                         <Accordion.Header>Boards</Accordion.Header>
                         <Accordion.Body>
@@ -115,8 +121,40 @@ class MyNavbar extends React.Component {
                   Settings
                 </Button>
                 <Modal show={this.state.showModal2}>
-                  <Modal.Header>Settings</Modal.Header>
-                  <Modal.Body></Modal.Body>
+                  <Modal.Header class="d-flex justify-content-center">
+                    <div>
+                      <h1>Settings</h1>
+                    </div>
+                  </Modal.Header>
+                  <Modal.Body>
+                    <ListGroup variant="flush">
+                      <ListGroup.Item>
+                        <Button className="btn-block mr-1 mt-1 btn-lg">
+                          Edit Profile
+                        </Button>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <Button className="btn-block mr-1 mt-1 btn-lg bg-light text-dark">
+                          ❤️ Favorites
+                        </Button>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <Button className="btn-block mr-1 mt-1 btn-lg bg-light text-dark">
+                          💻 Downloads
+                        </Button>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <Button className="btn-block mr-1 mt-1 btn-lg bg-light text-dark">
+                          🌐 Languages
+                        </Button>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <Button className="btn-block mr-1 mt-1 btn-lg bg-light text-dark">
+                          🌙 Dark Mode
+                        </Button>
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Modal.Body>
                   <Modal.Footer>
                     <Button
                       variant="danger"
@@ -142,7 +180,7 @@ class MyNavbar extends React.Component {
                 </a>
               </Nav>
             </Navbar.Collapse>
-          </Container>
+          </div>
         </Navbar>
       </div>
     );
